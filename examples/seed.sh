6245a26e8ecf0abc330ea3b0100ba3586694ef3d
Fiore Mario Vitale <mvitale@redhat.com>
2025-01-17 17:42:44 +0100

    DBZ-8459 Add transforms and predicates into example
    Signed-off-by: Fiore Mario Vitale <mvitale@redhat.com>

--- examples/seed.sh
+++ examples/seed.sh
@@ -3,4 +3,5 @@ port=${2:-8080}
 payloads_dir=${3:-payloads}
 http POST ${url}:${port}/api/sources @${payloads_dir}/source.json
 http POST ${url}:${port}/api/destinations @${payloads_dir}/destination.json
+http POST ${url}:${port}/api/transforms @${payloads_dir}/transform.json
 http POST ${url}:${port}/api/pipelines @${payloads_dir}/pipeline.json
