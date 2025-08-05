f2bb0148f898da29403927880e5b8f45a3ce64a4
Fiore Mario Vitale <mvitale@redhat.com>
2025-08-05 16:21:52 +0200

    DBZ-8836 Add a global exception handler to provide a common error message format

---
+++ debezium-platform-conductor/src/main/java/io/debezium/platform/error/GlobalExceptionMapper.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright Debezium Authors.
+ *
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.debezium.platform.error;
+
+import java.util.List;
+
+import jakarta.validation.ConstraintViolationException;
+import jakarta.ws.rs.core.Response;
+
+import org.jboss.resteasy.reactive.server.ServerExceptionMapper;
+
+public class GlobalExceptionMapper {
+
+    @ServerExceptionMapper(RuntimeException.class)
+    public Response mapRuntimeException(RuntimeException ex) {
+
+        // Try to unwrap a ConstraintViolationException
+        // Seems that Bean Validation on REST object don't work with Blazebit views.
+        Throwable cause = ex;
+        while (cause != null) {
+            if (cause instanceof ConstraintViolationException cve) {
+                List<String> violations = cve.getConstraintViolations().stream()
+                        .map(cv -> String.format(
+                                "%s: %s [%s]",
+                                cv.getPropertyPath(),
+                                cv.getMessage(),
+                                cv.getMessageTemplate()))
+                        .toList();
+
+                ErrorResponse response = new ErrorResponse("Validation failed", violations);
+                return Response.status(Response.Status.BAD_REQUEST).entity(response).build();
+            }
+            cause = cause.getCause();
+        }
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity(new ErrorResponse("An unexpected error happened", List.of(ex.getMessage())))
+                .build();
+    }
+
+    @ServerExceptionMapper(NotFoundException.class)
+    public Response mapNotFoundException(NotFoundException ex) {
+
+        return Response.status(Response.Status.NOT_FOUND)
+                .entity(new ErrorResponse("resource not found", List.of(ex.getMessage())))
+                .build();
+    }
+
+    @ServerExceptionMapper(IllegalArgumentException.class)
+    public Response mapIllegalArgumentExceptionException(IllegalArgumentException ex) {
+
+        return Response.status(Response.Status.BAD_REQUEST)
+                .entity(new ErrorResponse("resource not found", List.of(ex.getMessage())))
+                .build();
+    }
+
+    public record ErrorResponse(String error, List<String> details) {
+        public ErrorResponse(String error) {
+            this(error, List.of());
+        }
+    }
+}
