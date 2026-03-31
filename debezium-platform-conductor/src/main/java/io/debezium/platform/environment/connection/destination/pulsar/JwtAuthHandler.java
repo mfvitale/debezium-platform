085fc0ab77dbf773d03c77bacd934e754fd7e5fc
Philippe Camus <pxcamus@pm.me>
2026-03-31 14:57:39 +0200

    debezium/dbz#1083: Update license header in `JwtAuthHandler` to reference correct Apache Software License version
    Signed-off-by: Philippe Camus <pxcamus@pm.me>

--- debezium-platform-conductor/src/main/java/io/debezium/platform/environment/connection/destination/pulsar/JwtAuthHandler.java
+++ debezium-platform-conductor/src/main/java/io/debezium/platform/environment/connection/destination/pulsar/JwtAuthHandler.java
@@ -1,7 +1,7 @@
 /*
  * Copyright Debezium Authors.
  *
- * Licensed under the Apache License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache Software License version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.debezium.platform.environment.connection.destination.pulsar;
 
